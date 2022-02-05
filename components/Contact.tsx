5f0c0fb1cfb5f099c4a229f9c9723c39d7a07438
unknown <mawayalebo@gmail.com>
2022-02-05 16:59:56 +0200

    introducing cont us component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,3 +1,5 @@
+import ContactForm from "./ContactForm";
+
 function Contact() {
     return ( 
         <section>
@@ -5,8 +7,11 @@ function Contact() {
             <div>
                 <iframe width="100%" height="600" scrolling="no"  src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=12+Sirius+Rd,+Bedworth+Park,+Vereeniging,+1939/@-26.7156841,27.8825455,743m/data=!3m1!1e3!4m13!1m7!3m6!1s0x1e9458fb925b4355:0xd45fded469c88600!2s12+Sirius+Rd,+Bedworth+Park,+Vereeniging,+1939!3b1!8m2!3d-26.7156889!4d27.8847342!3m4!1s0x1e9458fb925b4355:0xd45fded469c88600!8m2!3d-26.7156889!4d27.8847342&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
             </div>
-            <div className="grid">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 bg-yellow-400 p-5">
+                <div className="grid  grid-cols-1 p-5">
 
+                </div>
+                <ContactForm/>
             </div>
         </section>
      );
--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -2,10 +2,10 @@ function ContactForm() {
     //contact us form component
     return ( 
         <form >
-            <div className="flex flex-col p-5 md:p-10 rounded-3xl shadow-md w-full">
+            <div className="flex flex-col p-5 md:p-10 rounded-3xl shadow-md w-full bg-white">
                 <div className="flex flex-1 items-center rounded-full ring ring-black px-3 md:px-10 py-2  m-2  md:space-x-5 overflow-hidden">
                     <label htmlFor="email" className="font-semibold whitespace-nowrap">Email : </label>
-                    <input type="text" name="email" id="email" className="form-input px-2 focus:ring-0 focus:border-0 flex-grow bg-transparent" placeholder="mawayalebo@gmail.com "/>
+                    <input type="text" name="email" id="email" className="form-input px-2 focus:ring-0 focus:border-0 flex-grow bg-transparent border-0" placeholder="mawayalebo@gmail.com "/>
                 </div>
                 <div className="flex flex-1 flex-col rounded-3xl ring ring-black  py-2  m-2 space-x-5 ">
                     <textarea name="message" id="message" cols={30} rows={10} className="form-textarea focus:ring-0 focus:border-0 flex-grow bg-transparent border-0" placeholder="Message here"></textarea>
